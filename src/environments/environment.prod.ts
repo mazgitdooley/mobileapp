31879f25ea6f35663de54379b9146fde8ada7bc2
Maria Dooley <mazgitdooley@users.noreply.github.com>
2026-04-18 19:37:30 +0100

    Add app entry, global styles, theme, and environment config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  tmdb: {
+    baseUrl: 'https://api.themoviedb.org/3',
+    imageBaseUrl: 'https://image.tmdb.org/t/p/w500',
+    apiKey: '8b24ee87252a6d40f468e7d5f417b120',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  tmdb: {
+    baseUrl: 'https://api.themoviedb.org/3',
+    imageBaseUrl: 'https://image.tmdb.org/t/p/w500',
+    apiKey: '8b24ee87252a6d40f468e7d5f417b120',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
